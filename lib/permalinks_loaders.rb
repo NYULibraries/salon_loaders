3e325e19a6a6f7a6ab420e18b7accb6f535b50ee
Eric Griffis <eric.griffis@nyu.edu>
2017-03-03 13:00:28 -0500

    Explicitly require files

--- lib/permalinks_loaders.rb
+++ lib/permalinks_loaders.rb
@@ -1,1 +1,9 @@
-Dir["#{File.dirname(__FILE__)}/lib/permalink_loaders/**/*.rb"].each { |f| load(f) }
+# Dir["#{File.dirname(__FILE__)}/lib/permalink_loaders/**/*.rb"].each { |f| load(f) }
+require 'lib_guides/api'
+require 'permalinks_loaders/permalink'
+require 'permalinks_loaders/sources/base'
+require 'permalinks_loaders/sources/libguides'
+require 'permalinks_loaders/sources/xerxes'
+
+module PermalinksLoaders
+end
