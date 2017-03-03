dee921995e93fd96dad56082578cab1979734d20
Eric Griffis <eric.griffis@nyu.edu>
2017-03-03 13:38:01 -0500

    Change require to require_relative

--- lib/permalinks_loaders.rb
+++ lib/permalinks_loaders.rb
@@ -1,9 +1,9 @@
 # Dir["#{File.dirname(__FILE__)}/lib/permalink_loaders/**/*.rb"].each { |f| load(f) }
 require 'lib_guides/api'
-require 'permalinks_loaders/permalink'
-require 'permalinks_loaders/sources/base'
-require 'permalinks_loaders/sources/libguides'
-require 'permalinks_loaders/sources/xerxes'
+require_relative 'permalinks_loaders/permalink'
+require_relative 'permalinks_loaders/sources/base'
+require_relative 'permalinks_loaders/sources/libguides'
+require_relative 'permalinks_loaders/sources/xerxes'
 
 module PermalinksLoaders
 end
