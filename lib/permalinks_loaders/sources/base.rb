c667ee6a806d9fb42b34f3e8a0e3e181bee32c63
Eric Griffis <eric.griffis@nyu.edu>
2017-03-03 15:05:42 -0500

    Add newline to end of files

--- lib/permalinks_loaders/sources/base.rb
+++ lib/permalinks_loaders/sources/base.rb
@@ -23,22 +23,8 @@ module PermalinksLoaders
       def to_txt
         permalinks.map do |permalink|
           "set #{permalink.key} #{permalink.url}"
-        end.join("\n")
+        end.join("\n") + "\n"
       end
-      #
-      # def self.connection_configured?
-      #   config = ActiveRecord::Base.configurations["old_arch"]
-      #   (config.try(:[], "adapter").present?)
-      # end
-      #
-      # self.establish_connection :old_arch if self.connection_configured?
-      # # ActiveRecord likes it when we tell it this is an abstract class only.
-      # self.abstract_class = true
-      #
-      # # Just to be safe
-      # def read_only?
-      #   true
-      # end
     end
   end
 end
