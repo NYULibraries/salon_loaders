152e049107c2a6be52ae5d3664f60b77b9625951
Eric Griffis <eric.griffis@nyu.edu>
2018-03-19 14:30:16 -0400

    Remove xerxes integration and associated dependencies

--- Gemfile
+++ Gemfile
@@ -1,9 +1,6 @@
 source "https://rubygems.org"
 
 gem 'rake', '>= 12'
-gem 'mysql2'
-gem 'activerecord'
-gem 'ox'
 gem 'lib_guides-api', github: "NYULibraries/lib_guides-api"
 
 group :test do
--- lib/salon_loaders.rb
+++ lib/salon_loaders.rb
@@ -1,7 +1,6 @@
 require_relative 'salon_loaders/permalink'
 require_relative 'salon_loaders/sources/base'
 require_relative 'salon_loaders/sources/libguides'
-require_relative 'salon_loaders/sources/xerxes'
 require_relative 'salon_loaders/sources/libguides_wrapper/az_asset'
 
 module SalonLoaders
