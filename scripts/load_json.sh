58e9983b77984ea7c010366de8b3e0a6f3dd62bd
Eric Griffis <eric.griffis@nyu.edu>
2018-04-06 15:16:56 -0400

    Remove redundant code in script

--- scripts/load_json.sh
+++ scripts/load_json.sh
@@ -1,19 +1,3 @@
-# /bin/bash
-rake salon_loaders:libguides:json
-export TOKEN=`curl -s -X POST \
-  -d grant_type=client_credentials \
-  -d client_id=$SALON_CLIENT_ID \
-  -d client_secret=$SALON_CLIENT_SECRET \
-  -d scope=admin \
-  https://dev.login.library.nyu.edu/oauth/token \
-  | sed 's/.*"access_token": *"\([^"]*\)".*}/\1/g'`
-curl -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $TOKEN" \
-  -X POST \
-  --data-binary "@libguides.json" \
-  https://persistent-dev.library.nyu.edu/reset_with_array
-unset TOKEN
-
 rake salon_loaders:libguides:json
 export TOKEN=`curl -s -X POST \
   -d grant_type=client_credentials \
