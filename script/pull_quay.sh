957ed5f302537075d35208d4a2a5dbacc123fc0c
Eric Griffis <eric.griffis@nyu.edu>
2019-03-19 13:10:05 -0400

    Fix quay pull

--- script/pull_quay.sh
+++ script/pull_quay.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
 
-docker pull quay.io/nyulibraries/salon_loaders:${CIRCLE_BRANCH//\//_} || quay.io/docker pull nyulibraries/salon_loaders:latest
+docker pull quay.io/nyulibraries/salon_loaders:${CIRCLE_BRANCH//\//_} || docker pull quay.io/nyulibraries/salon_loaders:latest
