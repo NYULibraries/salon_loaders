d65c5b1073e9c80ccc6469b10306588dd9cb176f
Eric Griffis <eric.griffis@nyu.edu>
2023-12-22 14:25:31 -0600

    Script - remove login (oauth) integration

--- script/load_json.sh
+++ script/load_json.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -ex
 bundle exec rake salon_loaders:libguides:json
-export TOKEN=`curl -s -X POST \
-  -d grant_type=client_credentials \
-  -d client_id=$SALON_CLIENT_ID \
-  -d client_secret=$SALON_CLIENT_SECRET \
-  -d scope=admin \
-  $SALON_LOGIN_TOKEN_URL \
-  | sed 's/.*\"access_token\": *\"\\([^\"]*\\)\".*}/\\1/g'`
+#export TOKEN=`curl -s -X POST \
+#  -d grant_type=client_credentials \
+#  -d client_id=$SALON_CLIENT_ID \
+#  -d client_secret=$SALON_CLIENT_SECRET \
+#  -d scope=admin \
+#  $SALON_LOGIN_TOKEN_URL \
+#  | sed 's/.*\"access_token\": *\"\\([^\"]*\\)\".*}/\\1/g'`
 curl -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $TOKEN" \
+  -H "Authorization: Bearer $BASIC_AUTH_TOKEN" \
   -X POST \
   --data "@libguides.json" \
   $SALON_LOAD_URL
-unset TOKEN
+#unset TOKEN
