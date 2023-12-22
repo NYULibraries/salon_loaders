f34477568a5561017d8759155c0ba3fa6c6df91c
Eric Griffis <eric.griffis@nyu.edu>
2023-12-22 15:07:32 -0600

    Script - ensure nonzero exit when API request fails

--- script/load_json.sh
+++ script/load_json.sh
@@ -7,7 +7,7 @@ bundle exec rake salon_loaders:libguides:json
 #  -d scope=admin \
 #  $SALON_LOGIN_TOKEN_URL \
 #  | sed 's/.*\"access_token\": *\"\\([^\"]*\\)\".*}/\\1/g'`
-curl -H "Content-Type: application/json" \
+curl --fail-with-body -H "Content-Type: application/json" \
   -H "Authorization: Basic $BASIC_AUTH_TOKEN" \
   -X POST \
   --data "@libguides.json" \
