4a30add48952601af7d93cb05ed411119d085a1b
Eric Griffis <eric.griffis@nyu.edu>
2023-12-22 14:54:32 -0600

    Script - fix basic auth header

--- script/load_json.sh
+++ script/load_json.sh
@@ -8,7 +8,7 @@ bundle exec rake salon_loaders:libguides:json
 #  $SALON_LOGIN_TOKEN_URL \
 #  | sed 's/.*\"access_token\": *\"\\([^\"]*\\)\".*}/\\1/g'`
 curl -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $BASIC_AUTH_TOKEN" \
+  -H "Authorization: Basic $BASIC_AUTH_TOKEN" \
   -X POST \
   --data "@libguides.json" \
   $SALON_LOAD_URL
